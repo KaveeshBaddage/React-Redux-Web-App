354f9893a79095c3cde6c49539c348c35463cfb4
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-12 20:53:18 +0530

    Add Dashboard.js file to create main view

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Dashboard extends Component {
+    render() {
+        return(
+            <div>
+                Dashboard
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
