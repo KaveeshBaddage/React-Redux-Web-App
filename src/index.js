7493e0a503606a9206946b73a59c64714aa6e7fb
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-12 20:52:30 +0530

    Add HashRouter and Route in to index.js to route app.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter, Route} from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter > 
+                <Route path="/" component={App} />        
+    </HashRouter>,
+     document.getElementById('root'));
 registerServiceWorker();
