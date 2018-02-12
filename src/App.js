cb494d2a418783c8a96fab695537593b5b38b80f
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-12 20:51:20 +0530

    Add  Route in to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello
-      </div>
+      <React.Fragment>
+            <Switch>
+              <Route path="/" component={Dashboard}/>
+            </Switch>
+      </React.Fragment>
     );
   }
 }
