c42fbd9f57d57ae2fb99c7544f37494a8a637c7e
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-12 21:39:13 +0530

    Add Navbar user defined component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from './../modules/navbar';
 
 class Dashboard extends Component {
     render() {
         return(
             <div>
+                <Navbar/>
                 Dashboard
             </div>
         )
