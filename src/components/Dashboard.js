ba8b1819fbe49bdaeeb0d2e273031f9633bf3a71
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-12 21:55:53 +0530

    Add  user defined <Sidebar/>  component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './../modules/navbar';
+import Sidebar from './../modules/sidebar';
 
 class Dashboard extends Component {
     render() {
         return(
             <div>
                 <Navbar/>
-                Dashboard
+                <Sidebar/>
             </div>
         )
     }
