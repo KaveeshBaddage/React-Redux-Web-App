e2dd593f45efef4eebf7a64244116452183b4122
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-14 23:58:30 +0530

    Refactor dashboard layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,11 +5,36 @@ import Sidebar from './../modules/sidebar';
 
 class Dashboard extends Component {
     render() {
-        return(
-            <div>
-                <Navbar/>
-                <Sidebar/>
-            </div>
+        return (
+            <React.Fragment>
+                {/* <div class="ui four column grid">
+                    <Navbar />
+                </div>
+                <div class="ui four column grid">
+                    <div class="row">
+                        <div class="two wide column">2</div>
+                        <div class="fourteen wide column">14</div>
+                    </div>
+                </div> */}
+                <div class="ui four column grid">
+                    <div class="row">
+                        <div class="column" style={{ height: 50 }}> <Navbar /></div>
+
+                    </div>
+                    <div class="row" style={{ height: 800 }}>
+                        <div class="three wide column"><Sidebar/></div>
+                        <div class="thirteen wide column"> 
+                            Dashboard content
+                        </div>
+                    </div>
+                </div>
+
+
+                {/* <Navbar/>
+                    <Sidebar/> */}
+
+
+            </React.Fragment>
         )
     }
 }
