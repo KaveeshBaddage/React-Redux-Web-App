afe2a307d416d3cbfab4c787174cb298210eaad8
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-20 08:04:31 +0530

    Add <Link > React-Router-Dom  component

--- src/modules/sidebar.jsx
+++ src/modules/sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Sidebar extends React.Component {
@@ -8,24 +9,24 @@ class Sidebar extends React.Component {
                 <div className="row">
                     <div className="column" id="sidebar">
                         <div className="ui secondary vertical fluid menu">
-                            <a className="active item">Dashboard</a>
-                            <a className="item">Reports</a>
-                            <a className="item">Analytics</a>
-                            <a className="item">Export</a>
+                            <Link to={'/test1'}><a className="active item">Dashboard</a> </Link>
+                            <Link to={'/reports'}>   <a className="item">Reports</a></Link>
+                            <Link to={'/analytics'}>    <a className="item">Analytics</a></Link>
+                            <Link to={'/export'}>    <a className="item">Export</a></Link>
                             <div className="ui hidden divider"></div>
-                            <a className="item">Nav item</a>
-                            <a className="item">Nav item again</a>
-                            <a className="item">One more nav</a>
-                            <a className="item">Another nav item</a>
-                            <a className="item">More navigation</a>
+                            <Link to={'/navtime1'}>    <a className="item">Nav item</a></Link>
+                            <Link to={'/navtime2'}>     <a className="item">Nav item again</a></Link>
+                            <Link to={'/navtime3'}>     <a className="item">One more nav</a></Link>
+                            <Link to={'/navtime4'}>    <a className="item">Another nav item</a></Link>
+                            <Link to={'/navtime5'}>    <a className="item">More navigation</a></Link>
                             <div className="ui hidden divider"></div>
-                            <a className="item">Macintosh</a>
-                            <a className="item">Linux</a>
-                            <a className="item">Windows</a>
+                            <Link to={'/macintosh'}>     <a className="item">Macintosh</a></Link>
+                            <Link to={'/linux'}>     <a className="item">Linux</a></Link>
+                            <Link to={'/windows'}>     <a className="item">Windows</a></Link>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
 
         );
     }
