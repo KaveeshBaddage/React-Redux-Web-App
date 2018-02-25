cdeb808b0865594bc05f93cda74c4ce138c8fb38
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-25 18:55:06 +0530

    Create endpoints

---
+++ src/services/endpoints.js
@@ -0,0 +1,7 @@
+const API = "api/v1/ap"
+
+export const LOGIN = API + "login";
+export const LOGOUT = API + "logout";
+export const PACKAGES = API + "pacakges";
+export const CUSTOMERS = API + "customers";
+export const COMPANIES = API + "companies";
