2d9f5e48c0eeade17b0fc22956e5edc8deffd6ec
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-25 18:53:06 +0530

    Import loginActions and LoginTypes

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import * as login from './login';
+import * as session from './session';
+
+export {login as loginTypes} 
+export const loginActions = login.default 
+
+
+export {session as sessionTypes}
+export const sessionActions = session.default 
