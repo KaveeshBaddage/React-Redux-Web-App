ad98dad78e8ab2260e4490e8ebd9ba265a18ca56
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-02-25 18:51:41 +0530

    Add login and logout route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Dashboard from './components/Dashboard'
+import Dashboard from './components/Dashboard';
+import Login from './components/login'
 
 
 class App extends Component {
@@ -8,7 +9,9 @@ class App extends Component {
     return (
       <React.Fragment>
             <Switch>
-              <Route path="/" component={Dashboard}/>
+              <Route exact path="/" component={Dashboard}/>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/logout" component={Login} />
             </Switch>
       </React.Fragment>
     );
