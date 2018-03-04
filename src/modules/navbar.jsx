c5b4947810a89ba6194b369647b9bc978d1e7d01
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-03-04 21:59:47 +0530

    Redirect to login page after Logout

--- src/modules/navbar.jsx
+++ src/modules/navbar.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route,Link, withRouter } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+
 import {
     Container,
     Header,
@@ -11,8 +15,36 @@ import {
     Divider,
     Input, Message
 } from 'semantic-ui-react';
+import { loginActions } from './../actions';
 
-class Navbar extends React.Component {
+
+class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.logoutUser = this.logoutUser.bind(this);
+
+    }
+
+    logoutUser(){
+        console.log("Calling logout")
+        this.props.logout();   
+    }
+
+    componentDidUpdate() {
+        if (this.props.isAuthenticated==false) {
+            console.log("Redirect to login");
+            this.props.history.push('/login');
+        }
+    }
+
+    componentDidMount() {
+        if (this.props.isAuthenticated==false) {
+            console.log("Redirect to login");
+            this.props.history.push('/login');
+        }
+    }
+
     render() {
         return (
             <div className="ui inverted huge borderless fixed fluid menu" style={{height:50}} id="navbar">
@@ -26,10 +58,25 @@ class Navbar extends React.Component {
                     <a className="item">Dashboard</a>
                     <a className="item">Settings</a>
                     <a className="item">Profile</a>
-                    <a className="item"  href="#/logout">Logout</a>
+                    <a className="item" onClick={this.logoutUser} >Sign Out </a>
+                         
                 </div>
             </div>
         );
     }
 }
-export default Navbar;
+
+function mapStateToProps(state) {
+    return {
+        ...state.Login,
+        isAuthenticated: state.session.isAuthenticated
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        ...loginActions
+    },dispatch);
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
